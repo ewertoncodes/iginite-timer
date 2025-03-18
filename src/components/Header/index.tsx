9288146bde7daf3d9ffadcf363e987fafc95b6d6
ewertoncodes <ewertonorg@gmail.com>
2025-03-17 22:59:31 -0300

    Header & Layout

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import { HeaderContainer } from './styles'
+
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { Scroll, Timer } from '@phosphor-icons/react'
+import { NavLink } from 'react-router-dom'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+
+      <img src={logoIgnite} alt="" />
+      <nav>
+        <NavLink to="/">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+
+  )
+}
